9d92b3b88770e4a4e34148a0ebac1024cf06da7d
Shrek <kapi.73@op.pl>
2024-06-09 01:14:33 +0200

    qr code added

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const homeBtn = document.querySelector('#homeBtn');
-const mediaBtn = document.querySelector('#mediaBtn');
+const projectTabs = document.querySelectorAll('.project');
 
+// if project div is in full sight on mobile, add class hightlight
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add('highlight');
+        } else {
+            entry.target.classList.remove('highlight');
+        }
+    });
+}, {threshold: [0, 1]});
+
+projectTabs.forEach((project) => {
+    observer.observe(project);
+});
