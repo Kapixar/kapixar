7621f164b12dfab234f3524be03a49fb6089aa30
Kapixar <85899949+Kapixar@users.noreply.github.com>
2025-03-14 02:00:36 +0100

    finalishing

--- scripts/script.js
+++ scripts/script.js
@@ -31,33 +31,49 @@ projectTabs.forEach((project) => {
 
 
 
-
 // placing blobs
 const blobAmount = 10;
-const viewPortHeight = 500;
 const paralaxContainer = document.querySelector('.paralaxContainer');
+const maxHeight = paralaxContainer.offsetHeight;
 
-for (let i = 0; i < blobAmount; i++) {
+function generateBlob(i, time) {
     const blobSvg = getBlobSvg({ growth: 2, edges: 6 });
     blobSvg.classList.add('floatingBlob', `layer${Math.round(Math.random() * 9)}`);
     // set random --blob-delay
-    blobSvg.style.setProperty('--blob-delay', `${Math.random() * 3}s`);
+    const delay = Math.random() * 2 + 1;
+    blobSvg.style.setProperty('--blob-delay', `${delay}s`);
     // set random --blob-speed
-    blobSvg.style.setProperty('--blob-speed', `${Math.random() * 10 + 10}s`);
+    blobSvg.style.setProperty('--blob-speed', `${time}s`);
     // set random --blob-scale
     blobSvg.style.setProperty('--blob-scale', Math.random() + 0.8);
     blobSvg.style.setProperty('--blob-rotate', `${Math.random() * 360}deg`);
 
-    const verticalPosition = (i / blobAmount) * viewPortHeight;
-    const horizontalPosition = (i % 2 === 0) ? Math.random() * 50 : 50 + Math.random() * 50;
+    const verticalPosition = Math.min(maxHeight, (Math.random() + paralaxContainer.scrollTop / window.outerHeight) * 400 - 100);
+    console.log(`verticalPosition: ${verticalPosition}`);
+    
+    const horizontalPosition = (i / blobAmount) * 200 - 50;
     blobSvg.style.setProperty('--blob-inset', `${verticalPosition}vh ${horizontalPosition}vw`);
 
     paralaxContainer.prepend(blobSvg);
+
+    setTimeout(() => {
+        blobSvg.remove();
+        generateBlob(i, time);
+    }, (time+delay) * 1000); // convert to milliseconds
 }
 
-const homeBlob = getLiquidBlobSvg({ growth: 2, edges: 10, color: "#FF4C29" });
-homeBlob.classList.add('homeBlob');
-document.querySelector('#home').prepend(homeBlob);
+
+for (let i = 0; i < blobAmount; i++) {
+    const time = Math.random() * 10 + 5;
+
+    setTimeout(() => {
+        generateBlob(i, time);
+    }, time * 100);
+}
+
+// const homeBlob = getLiquidBlobSvg({ growth: 2, edges: 10, color: "#FF4C29" });
+// homeBlob.classList.add('homeBlob');
+// document.querySelector('#home').prepend(homeBlob);
 
 
 
@@ -81,3 +97,41 @@ function setUpRotator(elements) {
         currentAngle += angle;
     });
 }
+
+// scaling the carousel
+
+const maxScale = 16;
+
+const carousels = document.querySelectorAll('.rotationContainer');
+carousels.forEach((carousel) => {
+    scaleCarousel(carousel);
+    window.addEventListener('resize', () => {
+        scaleCarousel(carousel);
+    });
+});
+
+function scaleCarousel(carousel) {
+    const screenWidth = window.innerWidth;
+    const carouselRealWidth = getComputedStyle(carousel, "after");
+    
+    
+    const scaleValue = screenWidth / parseInt(carouselRealWidth.width);
+
+    carousel.style.setProperty('scale', scaleValue > maxScale ? maxScale : scaleValue);
+}
+
+
+
+
+// trigger footer
+const footer = document.querySelector('footer');
+// if paralaxContainer is scrolled to the bottom, add class show to footer
+const scrollPosition = () => {
+    if (Math.abs(paralaxContainer.scrollHeight - paralaxContainer.clientHeight - paralaxContainer.scrollTop) <= window.innerHeight / 2) {
+        footer.classList.remove('hidden');
+    } else {
+        footer.classList.add('hidden');
+    }
+};
+
+paralaxContainer.addEventListener('scroll', scrollPosition);
